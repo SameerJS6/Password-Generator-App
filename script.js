54dffb245393f2ea6e862108983ba4428e3c3c49
Sameer Singh <sameersingh1234pok@gmail.com>
2022-12-15 21:20:33 +0530

    Proper Copy Functionality Added

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const PasswordValue = document.querySelector('[data-password-value]');
 CopyButton.addEventListener("click", CopyFunction);
 // Copy Function 
 function CopyFunction() {
-    const cb = navigator.clipboard;
-    cb.writeText(PasswordValue.innerText).then(() => CopyMessage.classList.remove("none"));
+    const copy = navigator.clipboard;
+    copy.writeText(PasswordValue.innerText).then(() => CopyMessage.classList.remove("none"));
 
     setTimeout(() => {
         CopyMessage.classList.add("none")
