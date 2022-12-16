ed4968d4c5482175cab3811a4d5418207e81c234
Sameer Singh <sameersingh1234pok@gmail.com>
2022-12-16 22:49:23 +0530

    Range Number Display Added

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const CopyButton = document.querySelector('[data-copy]');
 const CopyMessage = document.querySelector('[data-copy-message]');
 const ErrorMessage = document.querySelector("[data-error-message]");
 const PasswordValue = document.querySelector('[data-password-value]');
+const RangeInput = document.querySelector('.RangeInput')
+const RangeDisplay = document.querySelector('[data-range-number')
+
+// Range Display 
+RangeInput.addEventListener('input', ()=> {
+    let value;
+    value = RangeInput.value;
+    RangeDisplay.innerText = value;
+})
 
 CopyButton.addEventListener("click", CopyFunction);
 // Copy Function 
@@ -11,6 +20,5 @@ function CopyFunction() {
 
     setTimeout(() => {
         CopyMessage.classList.add("none")
-    }, 1000);
-    
+    }, 1000);   
 }
